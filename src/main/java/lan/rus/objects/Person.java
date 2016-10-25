4b0ad830bea8356bcfdae553b65e8da4cf138409
ruslan <ruslanindd@ua.fm>
2016-10-25 10:11:29 +0300

    1 from second

--- src/main/java/lan/rus/objects/Person.java
+++ src/main/java/lan/rus/objects/Person.java
@@ -1,5 +1,7 @@
 package lan.rus.objects;
 
+import lan.rus.types.petType;
+
 /**
  * Created by ruslan on 22.10.2016.
  */
@@ -8,17 +10,40 @@ public class Person {
     private String name;
     private int age;
     private Adress adress;
+    /**
+     * имя кошки
+     */
+    private String catName;
+    private petType pet;
 
     public Person(String name,int age,Adress adress){
         setName(name);
         setAge(age);
         setAdress(adress);
+
     }
+
+    public void addPet(petType pet){
+        setPet(pet);
+
+        petType pt;
+        switch (pt = getPet() ){
+
+            case DOG:
+                break;
+            case CAT:
+                break;
+            case PARROT:
+                break;
+        }
+    }
+
+
 
     public void print(){
         Utils.print(getName());
         Utils.print(String.valueOf(getAge()));
-       getAdress().print();
+        getAdress().print();
         Utils.print("============================");
     }
 
@@ -45,5 +70,21 @@ public class Person {
 
     public void setAdress(Adress adress) {
         this.adress = adress;
+    }
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+
+    public petType getPet() {
+        return pet;
+    }
+
+    public void setPet(petType pet) {
+        this.pet = pet;
     }
 }
---
+++ src/main/java/lan/rus/types/petType.java
@@ -0,0 +1,10 @@
+package lan.rus.types;
+
+/**
+ * Created by ruslan on 23.10.2016.
+ */
+public enum petType {
+    DOG,
+    CAT,
+    PARROT
+}
